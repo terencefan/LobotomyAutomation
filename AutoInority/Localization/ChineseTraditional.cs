a7bfbd9de9192fe270c44185c323eb27d5c16651
Terence Fan <stdrickforce@gmail.com>
2022-01-18 15:42:47 +0800

    Add ChineseTraditional (#2)

---
+++ AutoInority/Localization/ChineseTraditional.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+
+namespace AutoInority.Localization
+{
+    internal class TraditionalChinese : SimplifiedChinese
+    {
+        public override SystemLanguage Language => SystemLanguage.ChineseTraditional;
+    }
+}
