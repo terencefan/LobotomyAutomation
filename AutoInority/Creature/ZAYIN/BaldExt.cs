c4c5c568c5983905d5a815b6150397dacab23879
stdrickforce <stdrickforce@gmail.com>
2022-01-21 06:43:50 +0800

    You're bald...

---
+++ AutoInority/Creature/ZAYIN/BaldExt.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace AutoInority.Creature
+{
+    internal class BaldExt : BaseCreatureExt
+    {
+        private readonly MethodInfo _isBald;
+
+        public BaldExt(CreatureModel creature) : base(creature)
+        {
+            _isBald = typeof(Bald).GetMethod("IsBald");
+        }
+
+        public override bool CanWorkWith(AgentModel agent, SkillTypeInfo skill, out string message)
+        {
+            if (_creature.isOverloaded)
+            {
+                return base.CanWorkWith(agent, skill, out message);
+            }
+            return base.CanWorkWith(agent, skill, out message) && (bool)_isBald.Invoke(_creature.script, new object[] { agent });
+        }
+    }
+}
