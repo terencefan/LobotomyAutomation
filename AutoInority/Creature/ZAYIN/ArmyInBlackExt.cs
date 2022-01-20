9d8e6fb847fbe3942e31f98cb02c1288b82bc665
Terence Fan <stdrickforce@gmail.com>
2022-01-21 06:28:48 +0800

    Add Pink Corp support (#27)

---
+++ AutoInority/Creature/ZAYIN/ArmyInBlackExt.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoInority.Creature
+{
+    internal class ArmyInBlackExt : GoodNormalExt
+    {
+        public override SkillTypeInfo[] SkillSets => new SkillTypeInfo[] { Insight };
+
+        public ArmyInBlackExt(CreatureModel creature) : base(creature)
+        {
+        }
+    }
+}
