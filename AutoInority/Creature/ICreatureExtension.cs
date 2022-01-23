d1fa8ca5b0278e025b757a40e6c15acf7a4e182f
Terence Fan <tefa@microsoft.com>
2022-01-23 21:21:22 +0800

    Add LadyLookingAtWall & Scarecrow

--- AutoInority/Creature/ICreatureExtension.cs
+++ AutoInority/Creature/ICreatureExtension.cs
@@ -16,16 +16,18 @@ namespace AutoInority.Creature
 
         bool CheckConfidence(AgentModel agent, SkillTypeInfo skill);
 
+        float ConfidencePenalty(AgentModel agent, SkillTypeInfo skill);
+
         float GoodConfidence(AgentModel agent, SkillTypeInfo skill);
 
         float NormalConfidence(AgentModel agent, SkillTypeInfo skill);
 
         #endregion Confidence
 
-        bool TryGetEGOGift(out EquipmentTypeInfo gift);
-
         bool FarmFilter(AgentModel agent);
 
         IEnumerable<AgentModel> FindAgents(int distance);
+
+        bool TryGetEGOGift(out EquipmentTypeInfo gift);
     }
 }
---
+++ AutoInority/Creature/TETH/LadyLookingAtWallExt.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoInority.Creature
+{
+    internal class LadyLookingAtWallExt : ExpectGoodAndNormalExt
+    {
+        public override SkillTypeInfo[] SkillSets { get; } = new SkillTypeInfo[] { Instinct, Repression };
+
+        public LadyLookingAtWallExt(CreatureModel creature) : base(creature)
+        {
+        }
+    }
+}
